2db36b8082c2dd2db174ffdda4503969cbe36529
ebin0402 <ebin0402@gmail.com>
2020-05-30 23:58:48 +0530

    complete mean median mode

--- descriptive_statistics/mean.py
+++ descriptive_statistics/mean.py
@@ -1,2 +1,4 @@
-import intake,main
+import intake
 print(__name__)
+mean=sum(intake.nums)/len(intake.nums)
+print("Mean: {}".format(round(mean,2)))
--- descriptive_statistics/mode.py
+++ descriptive_statistics/mode.py
@@ -1,1 +1,17 @@
+
+import intake
+from collections import Counter 
 print(__name__)
+
+  
+# list of elements to calculate mode 
+n_num = intake.nums
+n = len(n_num) 
+data = Counter(n_num) 
+get_mode = dict(data) 
+mode = [k for k, v in get_mode.items() if v == max(list(data.values()))]  
+if len(mode) == n: 
+    get_mode = "No mode found"
+else: 
+    get_mode = "Mode is / are: " + ', '.join(map(str, mode))      
+print(get_mode) 
